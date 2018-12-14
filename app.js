8bbd293755922eaf596c5225ce5a8b385b068686
kapildownloader <abyss6271@gmail.com>
2018-12-14 09:48:29 +0530

    'session'

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(cookieParser())
     saveUninitialized:false
 })) */
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://locate-it-client1.herokuapp.com');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
     next();
